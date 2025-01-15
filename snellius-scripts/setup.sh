4b69b2220c6f1783e59706b0ccd75339e2697f77
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2025-01-15 11:42:23 +0100

    Add slurm and bash scripts for snellius

---
+++ snellius-scripts/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+MAX_STEPS=512 # 128 for testing, 512 for benchmarking
+
+MODEL_NAME="Llama-3-2-1b-Instruct"
+DATA_ROOT="/projects/0/prjs1019/torchtune"
+
+MODEL_DIR="${DATA_ROOT}/models/${MODEL_NAME}"
+OUTPUT_DIR="${DATA_ROOT}/outputs/${MODEL_NAME}"
+
+WANDB_DIR="${OUTPUT_DIR}/wandb"
+if [ ! -d "${WANDB_DIR}" ]; then
+    mkdir -p "${WANDB_DIR}"
+fi
+
+
+
